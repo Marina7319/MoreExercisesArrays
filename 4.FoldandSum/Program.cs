da0189a5f7e1f9bcbb80db9479e4d01dd4e1f36e
Marina7319 <marina-markova@abv.bg>
2022-08-18 14:51:34 +0300

    commit more exercises arrays

---
+++ 4.FoldandSum/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Linq;
+namespace _4.FoldandSum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] rowNums = Console.ReadLine().Split(separator: " ").Select(int.Parse).ToArray();
+            int k = rowNums.Length / 4;
+            int[] firstRow = new int[2 * k + 1];
+            int[] secondRow = new int[2 * k + 1];
+            int firstKCount = 0;
+            int secondRowCount = 0;
+            for (int i = rowNums.Length - 1; i >= 0; i--)
+            {
+                int currentNum = rowNums[i];
+            }
+            for (int firstK = k - 1; firstK >= 0; firstK--)
+            {
+                firstKCount++;
+                firstRow[firstKCount] = rowNums[firstK];
+            }
+            for (int lastK = rowNums.Length - 1; lastK >= rowNums.Length - k; lastK--)
+            {
+                firstKCount++;
+                firstRow[firstKCount] = rowNums[lastK];
+            }
+            for (int middleRow = k; middleRow < 3 * k; middleRow++)
+            {
+                secondRowCount++;
+                secondRow[secondRowCount] = rowNums[middleRow];
+            }
+
+            for (int sum = 1; sum <= 2 * k; sum++)
+            {
+                Console.Write(secondRow[sum] + firstRow[sum] + " ");
+            }
+        }
+    }
+}
---
+++ EncryptSortandPrintArray/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace EncryptSortandPrintArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numNames = int.Parse(Console.ReadLine());
+            int sum = 0;
+            int[] numbers = new int[numNames];
+            int tmp = 0;
+            for (int i = 0; i < numNames; i++)
+            {
+                string name = Console.ReadLine();
+                string[] nameLenght = new string[name.Length];
+                for (int currentName = 0; currentName < name.Length; currentName++)
+                {
+                    char letters = name[currentName];
+                    int sumVowel = 0;
+                    int sumConsonant = 0;
+                    if (name[currentName] == 'a' || name[currentName] == 'e' || name[currentName] == 'o' || name[currentName] == 'i' || name[currentName] == 'u' || name[currentName] == 'A' || name[currentName] == 'E' || name[currentName] == 'O' || name[currentName] == 'I' || name[currentName] == 'U')
+                    {
+                        int letter = (int)(name[currentName]);
+                        sumVowel = (name[currentName] * name.Length);
+                    }
+                    else
+                    {
+                        int letter = (int)(name[currentName]);
+                        sumConsonant = (name[currentName] / name.Length);
+                    }
+                    sum += (int)(sumVowel + sumConsonant);
+                }
+                numbers[i] = sum;
+                sum = 0;
+            }
+            for (int n = 0; n < numNames; n++)
+            {
+                for (int j = n + 1; j < numNames; j++)
+                {
+                    if (numbers[n] > numbers[j])
+                    {
+                        tmp = numbers[j];
+                        numbers[j] = numbers[n];
+                        numbers[n] = tmp;
+                    }
+                }
+            }
+            for (int l = 0; l < numNames; l++)
+            {
+                Console.WriteLine(numbers[l]);
+            }
+        }
+    }
+}
